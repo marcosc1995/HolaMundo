2b436f7fdb84434bd476a203dd3af3d4dc2fec6c
Marcos Caitano <marcos_c95@outlook.com>
2022-11-14 07:28:57 -0300

    Add project files.

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace HolaMundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hola mundo");
+            int resultado = Suma(20, 35);
+            Console.WriteLine(resultado);
+            int number = 69;
+            string test = "texto de prueba";
+            Console.WriteLine("esto es una prueba {0} {1}", number, test);
+            Console.Read();
+        }
+        public static int Suma(int num1, int num2)
+        {
+                       
+            return num1 + num2;
+        }
+        
+    }
+}
