8837e5ad9862f60cef34cd17a09efd56df00626e
Marcos_c95 <marcos_c95@outlook.com>
2022-11-14 08:43:20 -0300

    Ejercicios 1 y 2

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -7,19 +7,44 @@ namespace HolaMundo
     {
         static void Main(string[] args)
         {
+            //int temperatura = 22;
             //Console.WriteLine("hola mundo");
-            int resultado = Suma(20, 35);
-            Console.WriteLine(resultado);
-            int number = 69;
-            string test = "texto de prueba";
-            Console.WriteLine("esto es una prueba {0} {1}", number, test);
-            Console.Read();
+            //int resultado = Suma(20, 35);
+            //Console.WriteLine(resultado);
+            //int number = 69;
+            //string test = "texto de prueba";
+            //Console.WriteLine("esto es una prueba {0} {1}", number, test);
+            //Console.Read();
+
+            //EJERCICIO 1
+
+            //Console.WriteLine("ingresa una temperatura");
+            //string temperaruta = Console.ReadLine();
+            //int temp = int.Parse(temperaruta);
+            //if(temp > 30 )
+            //{
+            //    Console.WriteLine("Hacen {0} esta para ir a la playa", temp);
+            //    Console.Read();
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No tengo idea de que hacer");
+            //    Console.Read(); 
+            //}
+
+            // EJERCICIO 2 
+            //string testString = "69";
+            //int valorDeTest;
+            //bool test = int.TryParse(testString, out valorDeTest);
+            //Console.WriteLine(valorDeTest);
+            //Console.Read();
+            
         }
-        public static int Suma(int num1, int num2)
-        {
+        //public static int Suma(int num1, int num2)
+        //{
                        
-            return num1 + num2;
-        }
+        //    return num1 + num2;
+        //}
         
     }
 }
